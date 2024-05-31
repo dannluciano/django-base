80416958f44d18c999a2043bb95b42ff2aabf7c9
Dann Luciano <dannluciano@gmail.com>
2024-05-31 15:37:08 -0300

    Add Improvements on CoreUserAdmin

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,43 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from .models import User
 
-admin.site.register(User, UserAdmin)
+
+class CoreUserAdmin(UserAdmin):
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "created_at",
+        "updated_at",
+    )
+
+    fieldsets = (
+        (
+            None,
+            {"fields": ("username", "password", "uuid", "created_at", "updated_at")},
+        ),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    readonly_fields = ("uuid", "created_at", "updated_at")
+
+
+admin.site.register(User, CoreUserAdmin)
--- core/models.py
+++ core/models.py
@@ -6,3 +6,14 @@ from django.db import models
 
 class User(AbstractUser):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+
+class BaseModel(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+    class Meta:
+        abstract = True
