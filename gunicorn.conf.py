b26aae320d2fb17b5f0b1a045e2b47defa50e476
Dann Luciano <dannluciano@gmail.com>
2024-06-03 11:49:37 -0300

    Fix project_name Template Variable

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,6 +2,6 @@ import multiprocessing
 
 bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
-wsgi_app = "base.wsgi:application"
+wsgi_app = "{{project_name}}.wsgi:application"
 accesslog = "-"
 errorlog = "-"
