6dd2f22b3e31c1cec37ae5c746d19c867b9418dd
Dann Luciano <dannluciano@gmail.com>
2024-06-03 11:35:51 -0300

    Add E-mail Configs

--- {{ project_name }}/settings.py
+++ {{ project_name }}/settings.py
@@ -231,6 +231,14 @@ LOGGING = {
     },
 }
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
+EMAIL_TIMEOUT = config("EMAIL_TIMEOUT", default=10, cast=int)
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
 
 if DEBUG:
     INSTALLED_APPS += [
