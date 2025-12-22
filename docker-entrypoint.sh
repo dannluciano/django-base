ab7b1b1ae64380da79bb61bb6645be6cdc533641
Dann Luciano <dannluciano@gmail.com>
2025-12-21 22:59:23 -0300

    Fix Entry Point Double Execution

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-echo "==> Migrating Database"
-python3 manage.py migrate --noinput || exit 1
+if [ "$APP" = "web" ]; then
+    echo "==> Migrating Database"
+    python3 manage.py migrate --noinput || exit 1
 
-echo "==> Loading Data do Database"
-python3 manage.py loaddata seeds.json 
+    echo "==> Loading Data do Database"
+    python3 manage.py loaddata seeds.json 
 
-echo "==> Creating Bucket"
-python3 manage.py create_bucket
+    echo "==> Creating Bucket"
+    python3 manage.py create_bucket
+fi
 
 exec "$@"
