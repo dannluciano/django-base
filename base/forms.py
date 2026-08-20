a513f42b231746ee8008325143e862b2402669cd
Dann Luciano <dannluciano@gmail.com>
2026-08-20 14:52:32 -0300

    Fix ruff check

--- base/forms.py
+++ base/forms.py
@@ -1,4 +1,6 @@
 # forms.py
+from typing import ClassVar
+
 from django import forms
 from django.contrib.flatpages.models import FlatPage
 from django_prose_editor.widgets import ProseEditorWidget
@@ -8,6 +10,6 @@ class FlatPageForm(forms.ModelForm):
     class Meta:
         model = FlatPage
         fields = "__all__"
-        widgets = {
+        widgets: ClassVar[dict[str, forms.Widget]] = {
             "content": ProseEditorWidget(),
         }
